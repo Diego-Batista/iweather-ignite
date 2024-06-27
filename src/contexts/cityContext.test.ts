3b2940084b1ddfe618a9ce53d07661fa1a3f6801
Diego-Batista <diegobtistadev@gmail.com>
2024-06-27 09:31:03 -0300

    Atualizando o estado

--- src/contexts/cityContext.test.ts
+++ src/contexts/cityContext.test.ts
@@ -7,6 +7,12 @@ describe('Context: cityContext', () => {
   it('should be change selected city', async  () => {
     const { result } = renderHook(() => useCity(), { wrapper: CityProvider })
     
-    await waitFor(() => act(() => console.log(result.current))) 
+    await waitFor(() => act(() => result.current.handleChanceCity({
+      id: '1',
+      name: 'São Paulo',
+      latitude: 123,
+      longitude: 456
+    })))
+     
   })
 })
