27483760ca7e3af81509cf2b5538e835b4984801
Diego-Batista <diegobtistadev@gmail.com>
2024-07-04 13:35:53 -0300

    Finalizando o teste do Dashboard

--- src/screens/Dashboard/dashboard.test.tsx
+++ src/screens/Dashboard/dashboard.test.tsx
@@ -1,6 +1,6 @@
 import { mockCityAPIResponse } from "@__tests__/mocks/api/mockCityAPIResponse"
 import { mockWeatherAPIResponse } from "@__tests__/mocks/api/mockWeatherAPIResponse"
-import { render, screen, waitFor, waitForElementToBeRemoved } from "@__tests__/utils/customRender"
+import { act, fireEvent, render, screen, waitFor, waitForElementToBeRemoved } from "@__tests__/utils/customRender"
 import { saveStorageCity } from "@libs/asyncStorage/cityStorage"
 import { Dashboard } from "@screens/Dashboard"
 import { api } from "@services/api"
@@ -39,10 +39,21 @@ describe("Screen: Dashboard", () => {
       .mockResolvedValueOnce({ data: mockCityAPIResponse })
       .mockResolvedValueOnce({ data: mockWeatherAPIResponse })
 
-    const { debug } = render(<Dashboard />)
+    render(<Dashboard />)
 
     await waitForElementToBeRemoved(() => screen.queryByTestId('loading'))
 
-    debug()
+    const cityName = 'São Paulo'
+
+    await waitFor(() => act(() => {
+      const search = screen.getByTestId('search-input')
+      fireEvent.changeText(search, cityName)
+    }))
+
+    await waitFor(() => act(() => {
+      fireEvent.press(screen.getByText(cityName, { exact: false }))
+    }))
+
+    expect(screen.getByText(cityName, { exact: false })).toBeTruthy()
   })
 })
